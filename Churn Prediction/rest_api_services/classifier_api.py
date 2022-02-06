5e005d5c840dee0a5e7dc09caf0b9f390bbd611e
Duc Duong <qduc.duong@gmail.com>
2022-02-06 18:40:49 +0100

    Update classifier_api.py

--- Churn Prediction/rest_api_services/classifier_api.py
+++ Churn Prediction/rest_api_services/classifier_api.py
@@ -3,6 +3,7 @@ import pickle
 import os
 import numpy as np
 
+
 classifier = pickle.load(open('classifier.pickle', 'rb'))
 scaler = pickle.load(open('sc.pickle', 'rb'))
 
@@ -16,7 +17,7 @@ def prediction():
 
     prediction = classifier.predict(scaler.transform(np.array([[total_visits, average_time_spent]])))
 
-    return "The prediction is {}".format(prediction)
+    return "The prediction from GCP API is {}".format(prediction)
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=8005, debug=True)
