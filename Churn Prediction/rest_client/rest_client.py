8a2eeabcc6676ffdfc695f6ca2031a173c559857
Duc Duong <qduc.duong@gmail.com>
2022-02-09 23:10:54 +0100

    Commit
    3rd commit

--- Churn Prediction/rest_client/rest_client.py
+++ Churn Prediction/rest_client/rest_client.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-url = 'http://127.0.0.1:8000/model'
+url = 'http://35.230.13.35:8008/model'
 
 request_data = json.dumps({'total_visits': 100, 'average_time_spent': 100})
 response = requests.post(url, request_data)
